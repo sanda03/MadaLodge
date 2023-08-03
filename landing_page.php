2c27e0eeb4570673653c45519b42c1dd6d18bd3f
RobinaryTsinjo <128416113+RobinaryTsinjo@users.noreply.github.com>
2023-08-03 16:06:43 +0200

    Update landing_page.php
    Added css

--- landing_page.php
+++ landing_page.php
@@ -5,24 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recherche d'hôtel</title>
-    <link rel="stylesheet" href="landing_style.css">
+    <link rel="stylesheet" href="css\client_landing.css">
 </head>
 <body>
-    <h1>Recherche d'hôtel</h1>
-    <form method="post" action="search_results.php">
-        <label for="destination">Destination :</label>
-        <input type="text" id="destination" name="destination" required>
+    <header id="header">
+        <div class="container">
+            <div class="logo">
+                <img src="pictures\Capture-removebg-preview.png" alt="Logo">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 
-        <label for="start_date">Date d'arrivée :</label>
-        <input type="date" id="start_date" name="start_date" required>
+    <div class="search-section">
+        <h1>Vivez un séjour mémorable et magique</h1>
+        <form id="search-form" method="post" action="search_results.php">
+            <label for="destination">Destination :</label>
+            <input type="text" id="destination" name="destination" required>
 
-        <label for="end_date">Date de départ :</label>
-        <input type="date" id="end_date" name="end_date" required>
+            <label for="start_date">Date d'arrivée :</label>
+            <input type="date" id="start_date" name="start_date" required>
 
-        <label for="num_persons">Nombre de personnes :</label>
-        <input type="number" id="num_persons" name="num_persons" min="1" required>
+            <label for="end_date">Date de départ :</label>
+            <input type="date" id="end_date" name="end_date" required>
 
-        <input type="submit" value="Rechercher">
-    </form>
+            <label for="num_persons">Nombre de personnes :</label>
+            <input type="number" id="num_persons" name="num_persons" min="1" required>
+
+            <button type="submit">Rechercher</button>
+        </form>
+    </div>
+
 </body>
 </html>
