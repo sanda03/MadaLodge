eea9ea704062faf6532e98638076d8ad6528b50f
sanda03 <128069441+sanda03@users.noreply.github.com>
2023-08-08 10:37:17 +0200

    Update basic5.php

--- receptionist_pages/basic5.php
+++ receptionist_pages/basic5.php
@@ -2,8 +2,20 @@
 <html>
 <head>
     <title>Liste des réservations d'un client donné</title>
+    <link rel="stylesheet" href="recep.css">
 </head>
 <body>
+    <header>
+        <h1>Tableau de bord</h1>
+    </header>
+
+    <nav>
+        <a href="test_rep.php.php">Vérifier Paiement</a>
+        <a href="med_10.php">Total Réservations</a>
+        <a href="hard2.php">Chambres Libres</a>
+        <a href="pay.php">Réservations Client</a>
+        <a href="basic5.php">Liste Réservations</a>
+    </nav>
     <h1>Liste des réservations d'un client donné</h1>
 
     <form action="" method="POST">
@@ -55,4 +67,5 @@
     ?>
 </body>
 </html>
+
 
