d6f3992491ae7b1369780b943666f9599aaf94f6
sanda03 <128069441+sanda03@users.noreply.github.com>
2023-08-08 10:30:32 +0200

    Update recep.php

--- receptionist_pages/recep.php
+++ receptionist_pages/recep.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receptionist</title>
+    <link rel="stylesheet" href="recep.css">
+</head>
+<body>
+    <header>
+        <h1>Tableau de bord</h1>
+    </header>
 
+    <nav>
+        <a href="pay.php">Vérifier Paiement</a>
+        <a href="med_10.php">Total Réservations</a>
+        <a href="hard2.php">Chambres Libres</a>
+        <a href="basic8.php">Réservations Client</a>
+        <a href="basic5.php">Liste Réservations</a>
+    </nav>
+
+    <div>
+        <h1>Hello les receptionistes</h1>
+    </div>
+
+    
+</body>
+</html>
